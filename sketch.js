65541da665feda6ef7e3b0fde01737dcf7ffb3bb
Anderson Maia <antoniomaya27@hotmail.com>
2023-02-10 11:19:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 //Variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 10;
+let diametro = 15;
 let raio = diametro / 2;
 
 //velocidade da bolinha
@@ -19,6 +19,17 @@ let yRaqueteJ = 175;
 let xRaqueteC = 595;
 let yRaqueteC = 175;
 
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+
+
 
 function setup() {
   createCanvas(600, 400);
@@ -32,7 +43,9 @@ function draw() {
   moveBolinha();
   verificaColisaoBorda();
   movimentoRaqueteJ();
-  verificaColisaoRaquete()
+  verificaColisaoRaquete();
+  incluiPlacar();
+  marcaPonto();
   
   
 }
@@ -77,9 +90,44 @@ function movimentoRaqueteJ () {
 function verificaColisaoRaquete() {
   if (xBolinha - raio < xRaqueteJ + 5 && yBolinha - raio < yRaqueteJ + raqueteW && yBolinha + raio > yRaqueteJ) {
     velocidadeXBolinha *= -1;
+    raquetada.play();
   }
   
   if (xBolinha + raio > xRaqueteC && yBolinha - raio < yRaqueteC + raqueteW && yBolinha + raio > yRaqueteC) {
     velocidadeXBolinha *= -1;
   }
+}
+
+function incluiPlacar() {
+    stroke(255);
+    textAlign(CENTER);
+    textSize(16);
+    fill(color(255, 140, 0));
+    rect(150, 10, 40, 20);
+    fill(255);
+    text(meusPontos, 170, 26);
+    fill(color(255, 140, 0));
+    rect(450, 10, 40, 20);
+    fill(255);
+    text(pontosDoOponente, 470, 26);
+}
+
+function marcaPonto() {
+    if (xBolinha > 590) {
+        meusPontos += 1;
+    }
+    if (xBolinha < 10) {
+        pontosDoOponente += 1;
+    }
+}
+
+function preload() {
+    trilha = loadSound("trilha.mp3");
+    ponto = loadSound("ponto.mp3");
+    raquetada = loadSound("raquetada.mp3");
+}
+
+function setup() {
+    createCanvas(600, 400);
+    trilha.loop();
 }
